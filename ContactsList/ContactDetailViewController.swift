90e6f30b31b29dbb067fd5ae6646be8bbc878cc9
Naji Makhoul <naji.makhoul@hotmail.com>
2015-08-26 02:46:06 +0300

    add new files:  ContactDetailViewController and ContactsTableViewController

---
+++ ContactsList/ContactDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ContactDetailViewController.swift
+//  ContactsList
+//
+//  Created by Naji Makhoul on 26/08/15.
+//  Copyright (c) 2015 Naji Makhoul. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
