6589a9d41b20e5781ef9c2e0439957eff80bd9a3
Naji Makhoul <naji.makhoul@hotmail.com>
2015-08-26 02:58:28 +0300

    create new contact/ edit contact

--- ContactsList/ContactDetailViewController.swift
+++ ContactsList/ContactDetailViewController.swift
@@ -7,20 +7,103 @@
 //
 
 import UIKit
-
+import CoreData
 class ContactDetailViewController: UIViewController {
-
+    
+    let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+    
+    @IBOutlet var txtFirstName: UITextField!
+    @IBOutlet var txtLastName: UITextField!
+    @IBOutlet var txtPhone: UITextField!
+    @IBOutlet var txtEmail: UITextField!
+    
+    var contact: Contacts? = nil
+    var isEditMode: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if contact != nil {
+            setEditMode();
+            txtFirstName.text = contact?.firstName
+            txtLastName.text = contact?.lastName
+            txtPhone.text = contact?.phone
+            txtEmail.text = contact?.email
+        }
 
-        // Do any additional setup after loading the view.
     }
 
+    func setEditMode(){
+        var title: String = "Save"
+        if !isEditMode {
+            title = "Edit"
+        }
+        self.navigationItem.rightBarButtonItem?.title = title
+        txtFirstName.userInteractionEnabled  = isEditMode
+        txtLastName.userInteractionEnabled  = isEditMode
+        txtPhone.userInteractionEnabled  = isEditMode
+        txtEmail.userInteractionEnabled  = isEditMode
+        //isEditMode = false
+    }
+
+    
+    @IBAction func Save(sender: AnyObject){
+        if contact != nil{
+            if !isEditMode {
+                isEditMode = true
+                setEditMode()
+                txtFirstName.becomeFirstResponder()
+            }else{
+                editContact()
+                dismissViewController()
+            }
+            
+        }else{
+            createContact()
+            dismissViewController()
+            
+        }
+        
+    }
+    
+    @IBAction func cancel(sender: AnyObject) {
+        dismissViewController()
+    }
+    
+    func dismissViewController() {
+        navigationController?.popViewControllerAnimated(true)
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK:- Create contact
+    func createContact() {
+        
+        let entity = NSEntityDescription.entityForName("Contacts", inManagedObjectContext: managedObjectContext!)
+        let newContact = Contacts(entity: entity!, insertIntoManagedObjectContext: managedObjectContext)
+        newContact.firstName = txtFirstName.text
+        newContact.lastName = txtLastName.text
+        newContact.phone = txtPhone.text
+        newContact.email = txtEmail.text
+        
+        managedObjectContext?.save(nil)
+        
+    }
+    
+    // MARK:- Edit contact
+    func editContact() {
+        contact?.firstName = txtFirstName.text
+        contact?.lastName = txtLastName.text
+        contact?.phone = txtPhone.text
+        contact?.email = txtEmail.text
+        managedObjectContext?.save(nil)
+    }
+    
+
 
     /*
     // MARK: - Navigation
